651ad307ab2d30195a153aba825be79ce42480e7
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-03 17:34:30 -0500

    Basic root component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render () {
+        return (
+            <View>
+                <Text>Ésta es una app</Text>
+            </View>
+        );
+    }
+} 
+
+export default App;

3a8f00a1363f3ec9a8bc9464c311b73db5b41e48
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-08 11:29:28 -0500

    state for text is defined

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,13 +3,17 @@ import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+    state = { text: '' };
+
     render() {
         return (
             <View>
                 <Card>
                     <CardSection>
-                        <TextInput 
-                            style={{ height: 20, width: 200 }}
+                        <TextInput
+                            value={this.state.text}
+                            onChangeText={text => this.setState({ text: text })}
+                            style={{ height: 20, width: 100 }}
                         />
                     </CardSection>
                     <CardSection />

d4b86c92a948d266cbfb95e0ea56b0188011a853
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-10 05:42:29 -0500

    LoginForm is working

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
-class LoginForm extends Component {
-<<<<<<< HEAD
+class LoginForm extends Component {git 
     state = { text: '' };
 
     render() {
@@ -24,28 +23,7 @@ class LoginForm extends Component {
                     </Button>
                 </CardSection>
             </Card>
-=======
-    state = {text:''}
 
-    render() {
-        return (
-                <Card>
-                    <CardSection>
-                        <TextInput 
-                            value={this.state.text}
-                            onChangeText={text => this.setState({ text })}
-                            style={{height: 20, width: 100}} 
-                        />
-                    </CardSection>
-                    <CardSection />
-
-                    <CardSection>
-                        <Button>
-                            Log in
-                        </Button>
-                    </CardSection>
-                </Card>
->>>>>>> 787d95dc7097c6c6722f8ccf5531ba0baaf0dea8
         );
     }
 };
